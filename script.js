750b1032aae30c779861ab9ecbead6d36a400793
AZishan <hasiburbd24@gmail.com>
2022-02-10 21:42:04 +0600

    normal design complete

--- script.js
+++ script.js
@@ -8,3 +8,58 @@ const menuClose = document.getElementById('menuClose');
 menuClose.addEventListener('click', function(){
     document.getElementById('menuArea').style.display = "none";
 })
+
+$(".count").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 3000,
+          easing: "swing",
+          step: function (now) {
+            now = Number(Math.ceil(now)).toLocaleString('en');
+                                  $(this).text(now);
+          },
+        }
+      );
+  });
+
+  $(".countx").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 9000,
+          easing: "swing",
+          step: function (now) {
+            now = Number(Math.ceil(now)).toLocaleString('en');
+                                  $(this).text(now);
+          },
+        }
+      );
+  });
+
+  
+  $(".countz").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 1000,
+          easing: "swing",
+          step: function (now) {
+            now = Number(now).toLocaleString('en');
+                                  $(this).text(now);
+          },
+        }
+      );
+  });
